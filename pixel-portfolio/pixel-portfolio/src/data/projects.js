becf21f790f658465ad5db909526d8b2e78ea3ec
laxman mukhiya <51843238+laxmanmukhiya@users.noreply.github.com>
2026-04-03 09:25:19 +0530

    Update projects.js

--- pixel-portfolio/pixel-portfolio/src/data/projects.js
+++ pixel-portfolio/pixel-portfolio/src/data/projects.js
@@ -8,12 +8,25 @@ export const projects = [
     id: 1,
     title: "Career Recommendation System",
     description:
-      "An AI-powered system that analyzes user skills, interests, and market trends to recommend optimal career paths. Uses NLP to parse resumes and collaborative filtering for personalized suggestions.",
-    tech: ["Python", "Scikit-learn", "React", "FastAPI", "MongoDB"],
+      "Career recommendation system that uses TF-IDF Cosine Similarity",
+    tech: ["Python"],
     github: "https://github.com/laxmanmukhiya/Career-recommendation-model.git",
     live: "",
     status: "Completed",
     emoji: "🎯",
     highlight: true,
   },
+   {
+    id: 2,
+    title: "Chroma Match",
+    description:
+      "A browser-based color matching game that challenges users to recreate colors and gradients from memory, using RGB comparison and Euclidean distance to calculate matching accuracy and score.",
+    tech: ["HTML"],
+    github: "https://github.com/laxmanmukhiya/Chroma-Match.git",
+    live: "",
+    status: "Completed",
+    emoji: "🎯",
+    highlight: true,
+  },
+  
 ];
